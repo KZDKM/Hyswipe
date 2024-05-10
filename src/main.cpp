cbe5d956d0a2daf568e4fccde2969974965c60c4
KZdkm <tinghan.zhu@hotmail.com>
2024-05-10 22:19:06 +0100

    regress to hyprland 0.40.0

--- src/main.cpp
+++ src/main.cpp
@@ -100,9 +100,9 @@ void reloadConfig() {
     lockCursor = std::any_cast<Hyprlang::INT>(HyprlandAPI::getConfigValue(pHandle, "plugin:hyswipe:lockCursor")->getValue());
 }
 
-CSharedPointer<HOOK_CALLBACK_FN> configReloadHook;
-CSharedPointer<HOOK_CALLBACK_FN> mouseButtonHook;
-CSharedPointer<HOOK_CALLBACK_FN> mouseMoveHook;
+std::shared_ptr<HOOK_CALLBACK_FN> configReloadHook;
+std::shared_ptr<HOOK_CALLBACK_FN> mouseButtonHook;
+std::shared_ptr<HOOK_CALLBACK_FN> mouseMoveHook;
 
 APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE inHandle) {
     pHandle = inHandle;
